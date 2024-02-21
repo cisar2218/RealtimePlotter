f815d0d2bcd0dab669925e5b019e765974699e58
Dušan Jánsky <cisar2218@gmail.com>
2024-02-21 16:11:32 +0100

    dynamic point adding and color modification

--- RealtimePlotterUnityProject/Assets/VoxelRenderer.cs
+++ RealtimePlotterUnityProject/Assets/VoxelRenderer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class VoxelRenderer : MonoBehaviour
@@ -8,10 +9,23 @@ public class VoxelRenderer : MonoBehaviour
     ParticleSystem.Particle[] voxels;
     bool voxelsUpdated = false;
 
+    public int voxelsCount = 0;
     public int numPoints = 10000;
+    public int pointPerSec = 20;
     public float voxelScale = 0.1f;
     public float scale = 1f;
     public float updateDelay = 0.3f;
+
+    public UpdateMode mode;
+
+    Vector3[] lastPositions;
+    Color[] lastColors;
+
+    public enum UpdateMode
+    {
+        UpdateWhole,
+        IncremenetalAdding
+    }
 
     void Awake()
     {
@@ -20,10 +34,31 @@ public class VoxelRenderer : MonoBehaviour
 
     private IEnumerator UpdateLoop()
     {
-        while (true)
+        bool isUpdating = true;
+        while (isUpdating)
         {
-            Init();
-            yield return new WaitForSeconds(updateDelay);
+            switch (mode)
+            {
+                case UpdateMode.UpdateWhole:
+                    Init();
+                    yield return new WaitForSeconds(updateDelay);
+                    break;
+                case UpdateMode.IncremenetalAdding:
+                    voxelsCount += pointPerSec;
+                    if (voxelsCount > numPoints)
+                    {
+                        isUpdating = false;
+                        break;
+                    }
+                    Debug.Log(voxelsCount);
+                    Init(voxelsCount);
+                    yield return new WaitForSeconds(1.0f);
+                    break;
+                default:
+                    Debug.LogWarning("This {nameof(RenderMode)} not handled");
+                    isUpdating = false;
+                    break;
+            }
         }
     }
 
@@ -36,10 +71,39 @@ public class VoxelRenderer : MonoBehaviour
         for (int i = 0; i < numPoints; i++)
         {
             positions[i] = new Vector3(
-                Random.Range(-bound, bound), Random.Range(-bound/2, bound/2), Random.Range(-bound, bound)
+                Random.Range(-bound, bound), Random.Range(-bound / 2, bound / 2), Random.Range(-bound, bound)
                 );
             colors[i] = new Color(Random.value, Random.value, Random.value);
         }
+
+        SetVoxels(positions, colors);
+    }
+
+    void Init(int howMany)
+    {
+        float bound = 10;
+        Vector3[] positions = new Vector3[howMany];
+        Color[] colors = new Color[howMany];
+
+        for (int i = 0; i < howMany; i++)
+        {
+            if (lastPositions != null && i < lastPositions.Length)
+            {
+                positions[i] = lastPositions[i];
+                colors[i] = lastColors[i];
+            }
+            else
+            {
+                positions[i] = new Vector3(
+                    Random.Range(-bound, bound), Random.Range(-bound/2, bound/2), Random.Range(-bound, bound)
+                    );
+                float intensity = Mathf.Clamp((float)voxelsCount/(float)numPoints, 0.0f,1.0f);
+                Debug.Log("Red val:" + intensity);
+                colors[i] = new Color(intensity, 0.0f, 0.0f);
+            }
+        }
+
+        lastPositions = positions; lastColors = colors;
 
         SetVoxels(positions, colors);
     }
@@ -61,6 +125,7 @@ public class VoxelRenderer : MonoBehaviour
     void SetVoxels(Vector3[] positions, Color[] colors)
     {
         voxels = new ParticleSystem.Particle[positions.Length];
+        
 
         for (int i = 0; i < positions.Length; i++)
         {
